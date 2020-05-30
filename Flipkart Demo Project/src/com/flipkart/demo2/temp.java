0472505a6db0d3be6b2b6c2542d3bcbec2a96b00
Kunal Sachdeva <kunalsachdeva199@gmail.com>
2020-05-30 16:45:01 +0530

    new commit

---
+++ Flipkart Demo Project/src/com/flipkart/demo2/temp.java
@@ -0,0 +1,18 @@
+package com.flipkart.demo2;
+
+public class temp {
+	private int empId;
+	private String empName;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+}
