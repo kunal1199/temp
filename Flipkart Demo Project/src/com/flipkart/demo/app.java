4715ab04ccb29dbe1d48850b858e052db23b7ad0
Kunal Sachdeva <kunalsachdeva199@gmail.com>
2020-05-30 16:42:40 +0530

    new commit

--- Flipkart Demo Project/src/com/flipkart/demo/app.java
+++ Flipkart Demo Project/src/com/flipkart/demo/app.java
@@ -1,10 +1,15 @@
 package com.flipkart.demo;
 
+import com.flipkart.demo2.temp;
+
 class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		temp obj = new temp();
+		obj.setEmpId(9);
+		System.out.println(obj.getEmpId());
 	}
 
-}
+}	
